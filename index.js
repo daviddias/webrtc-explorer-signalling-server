5ddf77d0b7fe73f0e4746a06794a1bcc605c171d
David Dias <daviddias.p@gmail.com>
2014-11-10 14:27:02 +0000

    it works :D

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ var peerTable = {};
 //     socket: socket,
 //     invite: {
 //        peerId: <this peerId>
-//        predecessor: '<signaldata>',
-//        sucessor: '<signaldata>'
+//        signalData: {
+//          predecessor: '<signaldata>',
+//          sucessor: '<signaldata>'
+//        }
 //     },
 //     inviteReply: {
 //        peerId: <this peerId>,
-//        predecessor: '<signaldata>',
-//        sucessor: '<signaldata>'
+//        signalData: {
+//          predecessor: '<signaldata>',
+//          sucessor: '<signaldata>'
+//        }
 //   },
 //   ...
 // } 
@@ -35,13 +39,14 @@ function hapiStarted() {
 }
 
 function ioConnectionHandler(socket) {
-  // console.log('New peer connect with socket ID of: ', socket.id);
+  // console.log('New peer connect with {socketId: %s}', socket.id);
 
-  socket.on('s-join',  peerJoinRequest);
-  socket.on('s-response', peerJoinResponse);
+  socket.on('s-join',  peerJoin);
+  socket.on('s-join-next',  peerJoinNext); // for warmup
+  socket.on('s-response', peerJoinResponse); // for normal mode
   socket.on('disconnect', peerRemove); // socket.io own event
 
-  function peerJoinRequest (invite) {
+  function peerJoin (invite) {
     console.log('peer { peerId: %s } is requesting to join the network', invite.peerId);
 
     if (!bootstrap(invite, socket)) { return; }
@@ -50,21 +55,67 @@ function ioConnectionHandler(socket) {
     addPeerToTable(invite, socket);
 
     var p_s = predecessorAndSucessor(invite.peerId);
-    peerTable[p_s.predecessorId].socket.emit('c-predecessor', invite);
-    peerTable[p_s.sucessorId].socket.emit('c-sucessor', invite);
+    // peer with peerId will be the sucessor of the node with predecessorId and vice versa
+    peerTable[p_s.predecessorId].socket.emit('c-sucessor', invite);
+    peerTable[p_s.sucessorId].socket.emit('c-predecessor', invite);
   }
 
+  /// boostrap
+
+  function bootstrap(invite, socket) {
+    if (booted) { console.log('the chord has been booted'); return true; }
+
+    if (Object.keys(peerTable).length < 5) { addPeerToTable(invite, socket); } 
+
+    var peerIds = Object.keys(peerTable);
+    // console.log('We now have %d', peerIds.length);
+
+    if (peerIds.length >= 5) {
+      var sortedPeerTable = sortPeerTable();
+      peerIds.map(function (peerId) {   
+        var p_s = predecessorAndSucessor(peerId, sortedPeerTable);
+
+        var predecessorInvite = {
+          peerId: peerId,
+          signalData: peerTable[peerId].invite.signalData.sucessor 
+        };
+        
+        peerTable[p_s.sucessorId].socket.emit('c-warmup-predecessor', predecessorInvite);
+      });
+      booted = true;
+    } 
+    return false; // to not do the normal process
+  }
+
+  function peerJoinNext(sucessorInvite) {
+    peerTable[sucessorInvite.returnTo].socket.emit('c-warmup-sucessor', sucessorInvite);
+  }
+
+
+
+  /// Normal Response Process
+
   function peerJoinResponse (inviteReply) {
-    if (inviteReply.predecessor) {
+    if (inviteReply.signalData.predecessor) {
       console.log('received predecessor data for peer { peerId: %s }', inviteReply.peerId);
-      peerTable[inviteReply.peerId].inviteReply.predecessor = inviteReply.predecessor;
+      console.log('PEERS AVAILABLE: ', Object.keys(peerTable));
+
+      console.log('OBJECT', peerTable[inviteReply.peerId]);
+      peerTable[inviteReply.peerId].inviteReply.signalData.predecessor = inviteReply.signalData.predecessor;
     }
-    if (inviteReply.sucessor) {
-      console.log('received sucessor data for peer { peerId: %s }', inviteReply.peerId);
-      peerTable[inviteReply.peerId].inviteReply.sucessor = inviteReply.sucessor;
+
+    if (inviteReply.signalData.sucessor) {
+      console.log('received sucessor data for peer { peerId: %s }', inviteReply.peerId);     
+      console.log('PEERS AVAILABLE: ', Object.keys(peerTable));
+
+      console.log('OBJECT', peerTable[inviteReply.peerId]);
+
+
+      peerTable[inviteReply.peerId].inviteReply.signalData.sucessor = inviteReply.signalData.sucessor;
     }
+
     // if we have both responses from sucessor and predecessor, send to peer who iniatiated
-    if (peerTable[inviteReply.peerId].inviteReply.predecessor && peerTable[inviteReply.peerId].inviteReply.sucessor) {
+    if (peerTable[inviteReply.peerId].inviteReply.signalData.predecessor && peerTable[inviteReply.peerId].inviteReply.signalData.sucessor) {
       console.log('Sending c-response to peer with id:', inviteReply.peerId);
       peerTable[inviteReply.peerId].socket.emit('c-response', peerTable[inviteReply.peerId].inviteReply);
     }
@@ -77,38 +128,7 @@ function ioConnectionHandler(socket) {
   }
 }
 
-function bootstrap(invite, socket) {
-  if (booted) { 
-    console.log('the chord has been booted');
-    return true; 
-  }
 
-  if (Object.keys(peerTable).length < 5) {
-    addPeerToTable(invite, socket);
-  } 
-
-  var peerIds = Object.keys(peerTable);
-  // console.log('We now have %d', peerIds.length);
-
-  if (peerIds.length >= 5) {
-    var sortedPeerTable = sortPeerTable();
-    peerIds.map(function (peerId) {   
-      var p_s = predecessorAndSucessor(peerId, sortedPeerTable);
-           
-      // my predecessor is the other node sucessor and vice versa
-      var inviteReply = {
-        peerId: peerId,
-        predecessor: peerTable[p_s.predecessorId].invite.sucessor,
-        sucessor: peerTable[p_s.sucessorId].invite.predecessor
-      };
-
-      peerTable[peerId].socket.emit('c-response', inviteReply);
-    });
-    booted = true;
-  } 
-
-  return false; // to not do the normal process
-}
 
 function predecessorAndSucessor (peerId, sortedPeerTable) {
   var s = sortedPeerTable || sortPeerTable();
@@ -135,7 +155,6 @@ function predecessorAndSucessor (peerId, sortedPeerTable) {
   return r;
 }
 
-
 function sortPeerTable() {
   var sorted = Object
                 .keys(peerTable)
@@ -148,7 +167,6 @@ function sortPeerTable() {
                   if (aBig.greater(bBig)) {
                     return 1; 
                   }
-
                   return 0;
                 });
                 // .map(function(key){
@@ -161,7 +179,9 @@ function addPeerToTable(invite, socket) {
   peerTable[invite.peerId] = {
     socket: socket,
     invite: invite,
-    inviteReply: {}
+    inviteReply: {
+      signalData: {}
+    }
   };
   socketId_peerId[socket.id] = invite.peerId;
 }
