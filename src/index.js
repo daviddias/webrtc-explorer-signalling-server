aa373efb9ee34f29c2afdce0d2dd278734bda270
David Dias <daviddias.p@gmail.com>
2014-12-01 23:25:02 +0000

    fix race condition on new node joins

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ server.start(hapiStarted);
 
 function hapiStarted() {
   io.listen(server.listener).on('connection', ioConnectionHandler);
-  console.log('Signaling server has started on:', server.info.uri);
+  // console.log('Signaling server has started on:', server.info.uri);
 }
 
 function ioConnectionHandler(socket) {
@@ -31,30 +31,31 @@ function ioConnectionHandler(socket) {
   socket.on('disconnect', peerRemove); // socket.io own event
 
   function registerPeer(id) {
-    console.log('Registering peer with Id: ', id);
+    // console.log('Registering peer with Id: ', id);
     peerTable[id] = {
       socket: socket
     };
     socketIdpeerId[socket.id] = id;
-    console.log('peerTable: ', Object.keys(peerTable));
+    // console.log('peerTable: ', Object.keys(peerTable));
 
     if (Object.keys(peerTable).length > 2) {
       // warn a peer of new sucessor
       var sorted = utils.sortPeerTable(peerTable);
-      peerTable[utils.predecessorToId(id, sorted)]
+      var predecessorToId = utils.predecessorToId(id, sorted);
+      peerTable[predecessorToId]
         .socket.emit('c-new-sucessor-available', id);
     }
   }
 
   function sendOffer(offer) {
-    console.log('sendOffer');
+    // console.log('sendOffer');
     function twoOrMore() {
       if (Object.keys(peerTable).length < 2) {
-        console.log('--peerTable: ', Object.keys(peerTable));
+        // console.log('--peerTable: ', Object.keys(peerTable));
         return setTimeout(twoOrMore, 1000);
       }
 
-      console.log('sendOffer - flag');
+      // console.log('sendOffer - flag');
       var sorted = utils.sortPeerTable(peerTable);
 
       peerTable[utils.sucessorToId(offer.destId, sorted)]
@@ -64,12 +65,12 @@ function ioConnectionHandler(socket) {
   }
 
   function offerAccepted(offer) {
-    console.log('offerAccepted');
+    // console.log('offerAccepted');
     peerTable[offer.srcId].socket.emit('c-offer-accepted', offer);
   }
 
   function peerRemove() {
-    console.log('peer disconnected: ', socketIdpeerId[socket.id]);
+    // console.log('peer disconnected: ', socketIdpeerId[socket.id]);
     delete peerTable[socketIdpeerId[socket.id]];
     delete socketIdpeerId[socket.id];
   }
