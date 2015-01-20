98eff7466dab38129dfe9724f7e0ae463eaa28a5
David Dias <daviddias.p@gmail.com>
2015-01-20 00:25:48 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-webrtc-ring-signalling-server
+webrtc-explorer Signalling Server
 ==============================
 
-Please refer to the [webrtc-ring](https://github.com/diasdavid/webrtc-ring) for documentation. 
+> The `webrtc-explorer Signaling Server` implements the spec defined for the `webrtc-explorer` browser based DHT work properly, please refer to the [webrtc-explorer docs](https://github.com/diasdavid/webrtc-explorer) for instructions. 
 
 # Badgers
 
 [![Gitter](https://badges.gitter.im/Join Chat.svg)](https://gitter.im/diasdavid/webrtc-ring?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge) 
 [![Dependency Status](https://david-dm.org/diasdavid/webrtc-signalling-server.svg)](https://david-dm.org/diasdavid/webrtc-ring-signalling-server)
+
+# Spec
+
+## Events listented by the server
+
+- `s-register` - register a node on the DHT, assign him uuid and inform that Node of his Id through `c-registered`
+
+- `s-send-offer` - forward signaling data from one client that wants to connect to another client
+
+- `s-offer-accepted` - forward response signaling data
+
+## Events emitted by the server
+
+-`c-finger-update` - let a client know that he should update one of his fingers
+
+- `c-offer-accepted` - forward response signaling data
+
+- `c-accept-offer` - Ask a client to accept an offer from another client 
+
+- `c-registered` - tell a client he is registered and what is their Id
+
--- src/index.js
+++ src/index.js
@@ -1,77 +1,182 @@
-var hapi = require('Hapi');
+var Hapi = require('Hapi');
 var io = require('socket.io');
-var bigInt = require('big-integer');
-var utils = require('./lib/utils');
+var config = require('config');
+var Id = require('dht-id');
 
-var port = parseInt(process.env.PORT) || 9000;
-var server = new hapi.Server(port, {cors: true});
-var socketIdpeerId = {};
-var peerTable = {};
+var server = new Hapi.Server(config.get('hapi.options'));
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function(request, reply) {
-    reply('Signaling Server');
-  }
+server.connection({
+    port: config.get('hapi.port')
 });
 
-server.start(hapiStarted);
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+        reply('Signaling Server');
+    }
+});
 
-function hapiStarted() {
-  io.listen(server.listener).on('connection', ioConnectionHandler);
-  // console.log('Signaling server has started on:', server.info.uri);
+server.route({
+    method: 'GET',
+    path: '/dht',
+    handler: function(request, reply) {
+        reply(peers);
+    }
+});
+
+server.start(started);
+
+function started() {
+    io.listen(server.listener).on('connection', ioHandler);
+    console.log('Signaling server has started on:', server.info.uri);
 }
 
-function ioConnectionHandler(socket) {
+var peers = {};
+// id : {
+//     socketId:
+//     fingerTable: {
+//         row : {
+//             ideal: <fingerId>
+//             current: <fingerId>
+//         }
+//     }
+// }
 
-  socket.on('s-id', registerPeer);
-  socket.on('s-send-offer', sendOffer);
-  socket.on('s-offer-accepted', offerAccepted);
-  socket.on('disconnect', peerRemove); // socket.io own event
+var sockets = {};
+// socketId: socket
 
-  function registerPeer(id) {
-    // console.log('Registering peer with Id: ', id);
-    peerTable[id] = {
-      socket: socket
-    };
-    socketIdpeerId[socket.id] = id;
-    // console.log('peerTable: ', Object.keys(peerTable));
+function ioHandler(socket) {
 
-    if (Object.keys(peerTable).length > 2) {
-      // warn a peer of new sucessor
-      var sorted = utils.sortPeerTable(peerTable);
-      var predecessorToId = utils.predecessorToId(id, sorted);
-      peerTable[predecessorToId]
-        .socket.emit('c-new-sucessor-available', id);
+    // signalling server interactions
+    socket.on('s-register', registerPeer);
+    socket.on('disconnect', peerRemove); // socket.io own event
+    socket.on('s-send-offer', sendOffer);
+    socket.on('s-offer-accepted', offerAccepted);
+
+    function registerPeer() {
+        var peerId = new Id();
+        peers[peerId.toHex()] = {
+            socketId:  socket.id,
+            fingerTable: {}
+        };
+
+        sockets[socket.id] = socket;
+
+        socket.emit('c-registered', {peerId: peerId.toHex()});
+
+        console.log('registered new peer: ', peerId.toHex());
+
+        calculateIdealFingers(peerId);
+        updateFingers();
     }
-  }
 
-  function sendOffer(offer) {
-    // console.log('sendOffer');
-    function twoOrMore() {
-      if (Object.keys(peerTable).length < 2) {
-        // console.log('--peerTable: ', Object.keys(peerTable));
-        return setTimeout(twoOrMore, 1000);
-      }
-
-      // console.log('sendOffer - flag');
-      var sorted = utils.sortPeerTable(peerTable);
-
-      peerTable[utils.sucessorToId(offer.destId, sorted)]
-        .socket.emit('c-accept-offer', offer);
+    function calculateIdealFingers(peerId) {
+        var k = 1;
+        while (k <= config.get('explorer.n-fingers')) {
+            var ideal = (peerId.toDec() + Math.pow(2, k)) % Math.pow(2, 48);
+            peers[peerId.toHex()].fingerTable[k] = {
+                ideal: new Id(ideal).toHex(),
+                current: undefined
+            };
+            k++;
+        }
     }
-    twoOrMore();
-  }
 
-  function offerAccepted(offer) {
-    // console.log('offerAccepted');
-    peerTable[offer.srcId].socket.emit('c-offer-accepted', offer);
-  }
+    function updateFingers() {
+        if (Object.keys(peers) < 2) {
+            return;
+        }
 
-  function peerRemove() {
-    // console.log('peer disconnected: ', socketIdpeerId[socket.id]);
-    delete peerTable[socketIdpeerId[socket.id]];
-    delete socketIdpeerId[socket.id];
-  }
+        var sortedPeersId = Object.keys(peers).sort(function(a, b) {
+            var aId = new Id(a);
+            var bId = new Id(b);
+            if (aId.toDec() > bId.toDec()) {
+                return 1;
+            }
+            if (aId.toDec() < bId.toDec()) {
+                return -1;
+            }
+            if (aId.toDec() === bId.toDec()) {
+                console.log('error - There should never two identical ids');
+                process.exit(1);
+            }
+        });
+
+        sortedPeersId.forEach(function(peerId) {
+            Object.keys(peers[peerId].fingerTable).some(function(rowIndex) {
+                var fingerId = sucessorTo(peers[peerId]
+                                    .fingerTable[rowIndex]
+                                    .ideal, sortedPeersId);
+
+                if (peers[peerId].fingerTable[rowIndex].current !==
+                    fingerId) {
+
+                    peers[peerId].fingerTable[rowIndex].current = fingerId;
+
+                    sockets[peers[peerId].socketId].emit('c-finger-update', {
+                        rowIndex: rowIndex,
+                        fingerId: fingerId
+                    });
+                }
+
+                if (Object.keys(peers).length <
+                        config.get('explorer.min-peers')) {
+                    return true; // stops the loop, calculates only
+                    // for the first position (aka sucessor of the node
+                }
+            });
+        });
+
+        function sucessorTo (pretendedId, sortedIdList) {
+            pretendedId = new Id(pretendedId).toDec();
+            sortedIdList = sortedIdList.map(function(inHex) {
+                return new Id(inHex).toDec();
+            });
+
+            var sucessorId;
+            sortedIdList.some(function(value, index) {
+                if (pretendedId === value) {
+                    sucessorId = value;
+                    return true;
+                }
+
+                if (pretendedId < value) {
+                    sucessorId = value;
+                    return true;
+                }
+
+                if (index + 1 === sortedIdList.length) {
+
+                    sucessorId = sortedIdList[0];
+                    return true;
+                }
+            });
+
+            return new Id(sucessorId).toHex();
+        }
+    }
+
+    function peerRemove() {
+        Object.keys(peers).map(function(peerId) {
+            if (peers[peerId].socketId === socket.id) {
+                delete peers[peerId];
+                delete sockets[socket.id];
+                console.log('peer with Id: %s has disconnected', peerId);
+            }
+        });
+    }
+
+    // signalling mediation between two peers
+
+    function sendOffer(data) {
+        sockets[peers[data.offer.dstId].socketId]
+            .emit('c-accept-offer', data);
+    }
+
+    function offerAccepted(data) {
+        sockets[peers[data.offer.srcId].socketId]
+            .emit('c-offer-accepted', data);
+    }
+
 }
