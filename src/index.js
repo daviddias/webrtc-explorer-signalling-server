d6be029068df0a8e04a208b1b54a065c964b709d
David Dias <daviddias.p@gmail.com>
2015-10-27 17:05:48 +0000

    move to codestyle standard

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-var Hapi = require('Hapi');
-var io = require('socket.io');
-var config = require('config');
-var Id = require('dht-id');
+var Hapi = require('Hapi')
+var io = require('socket.io')
+var config = require('config')
+var Id = require('dht-id')
 
-var server = new Hapi.Server(config.get('hapi.options'));
+var server = new Hapi.Server(config.get('hapi.options'))
 
 server.connection({
-    port: config.get('hapi.port')
-});
+  port: config.get('hapi.port')
+})
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-        reply('Signaling Server');
-    }
-});
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Signaling Server')
+  }
+})
 
 server.route({
-    method: 'GET',
-    path: '/dht',
-    handler: function(request, reply) {
-        reply(peers);
-    }
-});
+  method: 'GET',
+  path: '/dht',
+  handler: function (request, reply) {
+    reply(peers)
+  }
+})
 
-server.start(started);
+server.start(started)
 
-function started() {
-    io.listen(server.listener).on('connection', ioHandler);
-    console.log('Signaling server has started on:', server.info.uri);
+function started () {
+  io.listen(server.listener).on('connection', ioHandler)
+  console.log('Signaling server has started on:', server.info.uri)
 }
 
-var peers = {};
+var peers = {}
 // id : {
 //     socketId:
 //     fingerTable: {
@@ -44,170 +44,165 @@ var peers = {};
 //     predecessorId
 // }
 
-var sockets = {};
+var sockets = {}
 // socketId: socket
 
-function ioHandler(socket) {
+function ioHandler (socket) {
+  // signalling server interactions
+  socket.on('s-register', registerPeer)
+  socket.on('disconnect', peerRemove) // socket.io own event
+  socket.on('s-send-offer', sendOffer)
+  socket.on('s-offer-accepted', offerAccepted)
 
-    // signalling server interactions
-    socket.on('s-register', registerPeer);
-    socket.on('disconnect', peerRemove); // socket.io own event
-    socket.on('s-send-offer', sendOffer);
-    socket.on('s-offer-accepted', offerAccepted);
-
-    function registerPeer() {
-        var peerId = new Id();
-        peers[peerId.toHex()] = {
-            socketId:  socket.id,
-            fingerTable: {}
-        };
-
-        sockets[socket.id] = socket;
-
-        socket.emit('c-registered', {peerId: peerId.toHex()});
-
-        console.log('registered new peer: ', peerId.toHex());
-
-        calculateIdealFingers(peerId);
-        updateFingers();
+  function registerPeer () {
+    var peerId = new Id()
+    peers[peerId.toHex()] = {
+      socketId: socket.id,
+      fingerTable: {}
     }
 
-    function calculateIdealFingers(peerId) {
-        var fingers = config.get('explorer.fingers');
-        var k = 1;
-        while (k <= fingers.length) {
-            var ideal = (peerId.toDec() + Math.pow(2, fingers[k - 1])) %
-                Math.pow(2, 48);
-            peers[peerId.toHex()].fingerTable[k] = {
-                ideal: new Id(ideal).toHex(),
-                current: undefined
-            };
-            k++;
-        }
+    sockets[socket.id] = socket
+
+    socket.emit('c-registered', {peerId: peerId.toHex()})
+
+    console.log('registered new peer: ', peerId.toHex())
+
+    calculateIdealFingers(peerId)
+    updateFingers()
+  }
+
+  function calculateIdealFingers (peerId) {
+    var fingers = config.get('explorer.fingers')
+    var k = 1
+    while (k <= fingers.length) {
+      var ideal = (peerId.toDec() + Math.pow(2, fingers[k - 1])) %
+        Math.pow(2, 48)
+      peers[peerId.toHex()].fingerTable[k] = {
+        ideal: new Id(ideal).toHex(),
+        current: undefined
+      }
+      k++
+    }
+  }
+
+  function updateFingers () {
+    if (Object.keys(peers).length < 2) {
+      return
     }
 
-    function updateFingers() {
-        if (Object.keys(peers).length < 2) {
-            return;
-        }
+    var sortedPeersId = Object.keys(peers).sort(function (a, b) {
+      var aId = new Id(a)
+      var bId = new Id(b)
+      if (aId.toDec() > bId.toDec()) {
+        return 1
+      }
+      if (aId.toDec() < bId.toDec()) {
+        return -1
+      }
+      if (aId.toDec() === bId.toDec()) {
+        console.log('error - There should never two identical ids')
+        process.exit(1)
+      }
+    })
 
-        var sortedPeersId = Object.keys(peers).sort(function(a, b) {
-            var aId = new Id(a);
-            var bId = new Id(b);
-            if (aId.toDec() > bId.toDec()) {
-                return 1;
-            }
-            if (aId.toDec() < bId.toDec()) {
-                return -1;
-            }
-            if (aId.toDec() === bId.toDec()) {
-                console.log('error - There should never two identical ids');
-                process.exit(1);
-            }
-        });
+    sortedPeersId.forEach(function (peerId) {
+      // predecessor
+      var predecessorId = predecessorTo(peerId, sortedPeersId)
 
-        sortedPeersId.forEach(function(peerId) {
+      if (peers[peerId].predecessorId !== predecessorId) {
+        sockets[peers[peerId].socketId].emit('c-predecessor', {
+          predecessorId: predecessorId
+        })
 
-            // predecessor
-            var predecessorId = predecessorTo(peerId, sortedPeersId);
+        peers[peerId].predecessorId = predecessorId
+      }
 
-            if (peers[peerId].predecessorId !== predecessorId) {
-                sockets[peers[peerId].socketId].emit('c-predecessor', {
-                    predecessorId: predecessorId
-                });
+      // sucessors
 
-                peers[peerId].predecessorId = predecessorId;
-            }
+      Object.keys(peers[peerId].fingerTable).some(function (rowIndex) {
+        var fingerId = sucessorTo(peers[peerId]
+          .fingerTable[rowIndex]
+          .ideal, sortedPeersId)
 
-            // sucessors
+        if (peers[peerId].fingerTable[rowIndex].current !==
+          fingerId) {
+          peers[peerId].fingerTable[rowIndex].current = fingerId
 
-            Object.keys(peers[peerId].fingerTable).some(function(rowIndex) {
-                var fingerId = sucessorTo(peers[peerId]
-                                    .fingerTable[rowIndex]
-                                    .ideal, sortedPeersId);
-
-                if (peers[peerId].fingerTable[rowIndex].current !==
-                    fingerId) {
-
-                    peers[peerId].fingerTable[rowIndex].current = fingerId;
-
-                    sockets[peers[peerId].socketId].emit('c-finger-update', {
-                        rowIndex: rowIndex,
-                        fingerId: fingerId
-                    });
-                }
-
-                if (Object.keys(peers).length <
-                        config.get('explorer.min-peers')) {
-                    return true; // stops the loop, calculates only
-                    // for the first position (aka sucessor of the node
-                }
-            });
-        });
-
-        function sucessorTo(pretendedId, sortedIdList) {
-            pretendedId = new Id(pretendedId).toDec();
-            sortedIdList = sortedIdList.map(function(inHex) {
-                return new Id(inHex).toDec();
-            });
-
-            var sucessorId;
-            sortedIdList.some(function(value, index) {
-                if (pretendedId === value) {
-                    sucessorId = value;
-                    return true;
-                }
-
-                if (pretendedId < value) {
-                    sucessorId = value;
-                    return true;
-                }
-
-                if (index + 1 === sortedIdList.length) {
-
-                    sucessorId = sortedIdList[0];
-                    return true;
-                }
-            });
-
-            return new Id(sucessorId).toHex();
+          sockets[peers[peerId].socketId].emit('c-finger-update', {
+            rowIndex: rowIndex,
+            fingerId: fingerId
+          })
         }
 
-        function predecessorTo(peerId, sortedIdList) {
-            var index = sortedIdList.indexOf(peerId);
-
-            var predecessorId;
-
-            if (index === 0) {
-                predecessorId = sortedIdList[sortedIdList.length - 1];
-            } else {
-                predecessorId = sortedIdList[index - 1];
-            }
-
-            return new Id(predecessorId).toHex();
+        if (Object.keys(peers).length <
+          config.get('explorer.min-peers')) {
+          return true // stops the loop, calculates only
+        // for the first position (aka sucessor of the node
         }
+      })
+    })
+
+    function sucessorTo (pretendedId, sortedIdList) {
+      pretendedId = new Id(pretendedId).toDec()
+      sortedIdList = sortedIdList.map(function (inHex) {
+        return new Id(inHex).toDec()
+      })
+
+      var sucessorId
+      sortedIdList.some(function (value, index) {
+        if (pretendedId === value) {
+          sucessorId = value
+          return true
+        }
+
+        if (pretendedId < value) {
+          sucessorId = value
+          return true
+        }
+
+        if (index + 1 === sortedIdList.length) {
+          sucessorId = sortedIdList[0]
+          return true
+        }
+      })
+
+      return new Id(sucessorId).toHex()
     }
 
-    function peerRemove() {
-        Object.keys(peers).map(function(peerId) {
-            if (peers[peerId].socketId === socket.id) {
-                delete peers[peerId];
-                delete sockets[socket.id];
-                console.log('peer with Id: %s has disconnected', peerId);
-            }
-        });
+    function predecessorTo (peerId, sortedIdList) {
+      var index = sortedIdList.indexOf(peerId)
+
+      var predecessorId
+
+      if (index === 0) {
+        predecessorId = sortedIdList[sortedIdList.length - 1]
+      } else {
+        predecessorId = sortedIdList[index - 1]
+      }
+
+      return new Id(predecessorId).toHex()
     }
+  }
 
-    // signalling mediation between two peers
+  function peerRemove () {
+    Object.keys(peers).map(function (peerId) {
+      if (peers[peerId].socketId === socket.id) {
+        delete peers[peerId]
+        delete sockets[socket.id]
+        console.log('peer with Id: %s has disconnected', peerId)
+      }
+    })
+  }
 
-    function sendOffer(data) {
-        sockets[peers[data.offer.dstId].socketId]
-            .emit('c-accept-offer', data);
-    }
+  // signalling mediation between two peers
 
-    function offerAccepted(data) {
-        sockets[peers[data.offer.srcId].socketId]
-            .emit('c-offer-accepted', data);
-    }
+  function sendOffer (data) {
+    sockets[peers[data.offer.dstId].socketId]
+      .emit('c-accept-offer', data)
+  }
 
+  function offerAccepted (data) {
+    sockets[peers[data.offer.srcId].socketId]
+      .emit('c-offer-accepted', data)
+  }
 }
--- tests/send-offer-test.js
+++ tests/send-offer-test.js
@@ -1,116 +1,116 @@
-var Lab = require('lab');
-var Code = require('code');
-var lab = exports.lab = Lab.script();
-var io = require('socket.io-client');
-var spawn = require('child_process').spawn;
-var request = require('request');
+var Lab = require('lab')
+var Code = require('code')
+var lab = exports.lab = Lab.script()
+var io = require('socket.io-client')
+var spawn = require('child_process').spawn
+// var request = require('request')
 
-var experiment = lab.experiment;
-var test = lab.test;
-var before = lab.before;
-var after = lab.after;
-var expect = Code.expect;
+var experiment = lab.experiment
+var test = lab.test
+var before = lab.before
+var after = lab.after
+var expect = Code.expect
 
-experiment(':', function() {
-    var server;
-    var c1Io;
-    var c2Io;
+experiment(':', function () {
+  var server
+  var c1Io
+  var c2Io
 
-    var c1Id;
-    var c2Id;
+  var c1Id
+  var c2Id
 
-    var c1FingerTable = {};
-    var c2FingerTable = {};
+  var c1FingerTable = {}
+  var c2FingerTable = {}
 
-    var ioOptions = {
-        transports: ['websocket'],
-        'force new connection': true
-    };
-    var url = 'http://localhost:9000';
+  var ioOptions = {
+    transports: ['websocket'],
+    'force new connection': true
+  }
+  var url = 'http://localhost:9000'
 
-    before(function(done) {
-        server = spawn('node', ['./src/index.js']);
-        server.stdout.on('data', function(data) {
-            console.log('\n stdout: ' +  data);
-        });
-        server.stderr.on('data', function(data) {
-            console.log('\n stderr: ' +  data);
-        });
-        setTimeout(function() { done(); }, 1000);
-    });
+  before(function (done) {
+    server = spawn('node', ['./src/index.js'])
+    server.stdout.on('data', function (data) {
+      console.log('\n stdout: ' + data)
+    })
+    server.stderr.on('data', function (data) {
+      console.log('\n stderr: ' + data)
+    })
+    setTimeout(function () { done() }, 1000)
+  })
 
-    after(function(done) {
-        c1Io.disconnect();
-        c2Io.disconnect();
+  after(function (done) {
+    c1Io.disconnect()
+    c2Io.disconnect()
 
-        setTimeout(function() {
-            server.on('close', function(code) {
-                console.log('exited: ' +  code);
-            });
+    setTimeout(function () {
+      server.on('close', function (code) {
+        console.log('exited: ' + code)
+      })
 
-            server.kill();
-        }, 500);
-    });
+      server.kill()
+    }, 500)
+  })
 
-    test('connect 2 clients', function(done) {
-        var count = 0;
-        c1Io = io.connect(url, ioOptions);
-        c2Io = io.connect(url, ioOptions);
+  test('connect 2 clients', function (done) {
+    var count = 0
+    c1Io = io.connect(url, ioOptions)
+    c2Io = io.connect(url, ioOptions)
 
-        c1Io.on('connect', connected);
-        c2Io.on('connect', connected);
+    c1Io.on('connect', connected)
+    c2Io.on('connect', connected)
 
-        function connected() {
-            count += 1;
-            if (count === 2) {
-                setTimeout(done, 1000);
-            }
-        }
-    });
+    function connected () {
+      count += 1
+      if (count === 2) {
+        setTimeout(done, 1000)
+      }
+    }
+  })
 
-    test('register 2 peer peers', {timeout: 60 * 1000}, function(done) {
-        c1Io.once('c-registered', function(data) {
-            c1Id = data.peerId;
-            expect(c1Id).to.be.a.string();
-        });
+  test('register 2 peer peers', {timeout: 60 * 1000}, function (done) {
+    c1Io.once('c-registered', function (data) {
+      c1Id = data.peerId
+      expect(c1Id).to.be.a.string()
+    })
 
-        c2Io.once('c-registered', function(data) {
-            c2Id = data.peerId;
-            expect(c2Id).to.be.a.string();
-        });
+    c2Io.once('c-registered', function (data) {
+      c2Id = data.peerId
+      expect(c2Id).to.be.a.string()
+    })
 
-        c1Io.on('c-finger-update', function(data) {
-            expect(data.rowIndex).to.be.a.string();
-            expect(data.fingerId).to.be.a.string();
-            c1FingerTable[data.rowIndex] = data.fingerId;
-        });
+    c1Io.on('c-finger-update', function (data) {
+      expect(data.rowIndex).to.be.a.string()
+      expect(data.fingerId).to.be.a.string()
+      c1FingerTable[data.rowIndex] = data.fingerId
+    })
 
-        c2Io.on('c-finger-update', function(data) {
-            expect(data.rowIndex).to.be.a.string();
-            expect(data.fingerId).to.be.a.string();
-            c2FingerTable[data.rowIndex] = data.fingerId;
-        });
+    c2Io.on('c-finger-update', function (data) {
+      expect(data.rowIndex).to.be.a.string()
+      expect(data.fingerId).to.be.a.string()
+      c2FingerTable[data.rowIndex] = data.fingerId
+    })
 
-        c1Io.emit('s-register', {});
-        c2Io.emit('s-register', {});
+    c1Io.emit('s-register', {})
+    c2Io.emit('s-register', {})
 
-        setTimeout(done, 1000);
-    });
+    setTimeout(done, 1000)
+  })
 
-    test('send offer', function(done) {
-        var offer = {
-            srcId: c1Id,
-            dstId: c2Id,
-            signalData: 'some stuff'
-        };
-        c1Io.emit('s-send-offer', {offer: offer});
-        c1Io.once('c-offer-accepted', function(data) {
-            done();
-        });
+  test('send offer', function (done) {
+    var offer = {
+      srcId: c1Id,
+      dstId: c2Id,
+      signalData: 'some stuff'
+    }
+    c1Io.emit('s-send-offer', {offer: offer})
+    c1Io.once('c-offer-accepted', function (data) {
+      done()
+    })
 
-        c2Io.once('c-accept-offer', function(data) {
-            data.offer.signalDataReturn = 'some more stuff';
-            c2Io.emit('s-offer-accepted', data);
-        });
-    });
-});
+    c2Io.once('c-accept-offer', function (data) {
+      data.offer.signalDataReturn = 'some more stuff'
+      c2Io.emit('s-offer-accepted', data)
+    })
+  })
+})
